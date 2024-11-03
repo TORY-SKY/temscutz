4620adeeaafbe4c661f5695f3e1807c4476ffa7f
TORY-SKY <ezevictor559@gmail.com>
2024-11-03 01:25:31 +0100

    barber shop landing page"

--- src/components/Landingpage.tsx
+++ src/components/Landingpage.tsx
@@ -1,50 +1,51 @@
+import heroImg from "../assets/images/pngwing.com.png";
+import Tems from "../assets/Temshaircut/hair1.jpg";
 const Landingpage = () => {
   return (
     <div className="landingpage-container">
-      <div className="mission-Hero">
-        <h1 className="mission-Hero-text">
-          Always Make Room for Beauty in your Life
-        </h1>
+      <div className="contents">
+        <img
+          src={heroImg}
+          alt="hero-image"
+          style={{ width: "200px", height: "auto" }}
+        />
+        <div className="contents-words">
+          <h1 className="hero-text">
+            Redefine Your Look, Elevate Your Confidence
+          </h1>
+          <p>
+            Experience precision cuts and timeless grooming. Step into
+            confidence with every visit.
+          </p>
+        </div>
       </div>
-      <div className="mission-explained">
-        <p>Always Make Room for Beauty in your Life</p>
-      </div>
-      <div className="clickable-btns">
-        <button>
-          Get started for free
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-4"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
-            />
-          </svg>
-        </button>
-        <button>
-          I have a question
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 0 1-.825-.242m9.345-8.334a2.126 2.126 0 0 0-.476-.095 48.64 48.64 0 0 0-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0 0 11.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155"
-            />
-          </svg>
-        </button>
-      </div>
+      {/* section two */}
+      <section className="content-two">
+        <div className="content-two-container">
+          <div className="content-two-left-side">
+            <h1 className="hero-text-two">
+              Tailored Grooming for the Mordern Gentlemen
+            </h1>
+            <p>
+              Where classic techniques meet modern style. Discover a grooming
+              experience designed for your unique look and comfort.
+            </p>
+            <button
+              className="about-us-btn"
+              style={{
+                border: "1px solid wheat",
+                padding: "8px",
+                width: "120px",
+              }}
+            >
+              About Us
+            </button>
+          </div>
+          <div className="content-two-right-side">
+            <img src={Tems} alt="tems at work" style={{ width: "20rem" }} />
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,38 +1,43 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import logo from "../assets/images/tem.png";
+
 const Navbar: FC = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false);
+  const toggleSidebar = () => {
+    setIsSidebarOpen((prev) => !prev);
+  };
+
   return (
     <>
       <header className="navigation-bar">
         <nav className="classA-nav">
-          <ul className="nav-links">
-            <li>
-              <a href="" className="a-tags">
-                Barber Shop
-              </a>
-            </li>
-            <li>
+          <ul className={`side-bar ${isSidebarOpen ? "Open" : "Close"}`}>
+            <CloseIcon className="close-icon" onClick={toggleSidebar} />
+            <li className="li">
               <a href="/home" className="a-tags">
                 Home
               </a>
             </li>
-            <li>
+            <li className="li">
               <a href="/aboutus" className="a-tags">
                 About Us
               </a>
             </li>
-            <li>
+            <li className="li">
               <a href="/service" className="a-tags">
                 Service
               </a>
             </li>
-            <li>
+            <li className="li">
               <a href="/shop" className="a-tags">
                 Shop
               </a>
             </li>
             <li
-              className="shopping-cart"
+              className="shopping-cart "
               style={{
                 marginLeft: "30px",
                 marginRight: "30px",
@@ -41,8 +46,55 @@ const Navbar: FC = () => {
               <ShoppingCartIcon />
               <i className="cart-items">10</i>
             </li>
-            <li className="reserve-btn">
+            <li className="reserve-btn li">
               <button className="a-tags">Reserve Now</button>
+            </li>
+          </ul>
+          <ul className="nav-links">
+            <li className="hideOnMobile">
+              <a href="" className="a-tags">
+                <img
+                  src={logo}
+                  alt="barber logo"
+                  style={{ width: "6rem", height: "auto" }}
+                />
+              </a>
+            </li>
+            <li className="hideOnMobile">
+              <a href="/home" className="a-tags">
+                Home
+              </a>
+            </li>
+            <li className="hideOnMobile">
+              <a href="/aboutus" className="a-tags">
+                About Us
+              </a>
+            </li>
+            <li className="hideOnMobile">
+              <a href="/service" className="a-tags">
+                Service
+              </a>
+            </li>
+            <li className="hideOnMobile">
+              <a href="/shop" className="a-tags">
+                Shop
+              </a>
+            </li>
+            <li
+              className="shopping-cart hideOnMobile"
+              style={{
+                marginLeft: "30px",
+                marginRight: "30px",
+              }}
+            >
+              <ShoppingCartIcon />
+              <i className="cart-items">10</i>
+            </li>
+            <li className="reserve-btn hideOnMobile">
+              <button className="a-tags">Reserve Now</button>
+            </li>
+            <li onClick={toggleSidebar} className="Menu-Icon">
+              <MenuIcon />
             </li>
           </ul>
         </nav>
