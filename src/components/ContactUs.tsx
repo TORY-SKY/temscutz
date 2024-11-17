9656801e5f5a039b9897c6c9ba816d47397c172f
TORY-SKY <141348591+TORY-SKY@users.noreply.github.com>
2024-11-17 10:32:28 +0100

    Update ContactUs.tsx
    Dependencies config

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -1,5 +1,5 @@
 import React, { useRef } from "react";
-import emailjs from "emailjs-com";
+import emailjs from "@emailjs/browser";
 import "./CSS/contactus.css";
 
 const ContactUs: React.FC = () => {
